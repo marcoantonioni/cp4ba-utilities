84f641ade360b843c61c40abe6ca1380d338762e
marcoantonioni <antonioni.marco@gmail.com>
2023-12-23 13:18:02 +0100

    update scripts

--- reboot-ocp-cluster-nodes/reboot-nodes.sh
+++ reboot-ocp-cluster-nodes/reboot-nodes.sh
@@ -3,13 +3,22 @@
 _me=$(basename "$0")
 
 _CTRL_PLANE=false
+_WORKERS=false
+
+#--------------------------------------------------------
+_CLR_RED="\033[0;31m"   #'0;31' is Red's ANSI color code
+_CLR_GREEN="\033[0;32m"   #'0;32' is Green's ANSI color code
+_CLR_YELLOW="\033[1;32m"   #'1;32' is Yellow's ANSI color code
+_CLR_BLUE="\033[0;34m"   #'0;34' is Blue's ANSI color code
+_CLR_NC="\033[0m"
 
 #--------------------------------------------------------
 # read command line params
-while getopts c flag
+while getopts cw flag
 do
     case "${flag}" in
         c) _CTRL_PLANE=true;;
+        w) _WORKERS=true;;
     esac
 done
 
@@ -37,7 +46,7 @@ listNodes () {
 rebootNode () {
   NODE_NAME=$1
   echo "#----------------------------------"
-  echo "Cordoning and rebooting node: "$NODE_NAME
+  echo -e "${_CLR_GREEN}Cordoning and rebooting node: '${NODE_NAME}'${_CLR_NC}"
   oc adm cordon $NODE_NAME
   oc adm drain --ignore-daemonsets --delete-emptydir-data --force --disable-eviction $NODE_NAME
   echo "Wait $DELAY_BEFORE_REBOOT seconds before rebooting the node..."
@@ -68,16 +77,25 @@ DELAY_AFTER_REBOOT=60
 TMP_OUT_FILE="./nodes-list"
 
 echo "#======================================="
-listNodes "worker" ${TMP_OUT_FILE}
-echo "Nodes for worker:"
-cat ${TMP_OUT_FILE}
-rebootNodes ${TMP_OUT_FILE}
-rm ${TMP_OUT_FILE}
+echo "Reboot nodes: workers[${_WORKERS}] control[${_CTRL_PLANE}]"
+echo ""
+if [[ "${_WORKERS}" = "false" ]] && [[ "${_CTRL_PLANE}" = "false" ]]; then
+  echo -e "${_CLR_RED}Nothing to do, use ${_CLR_YELLOW}'-c'${_CLR_RED} for control-plane, ${_CLR_YELLOW}'-w'${_CLR_RED} for workers${_CLR_NC}"
+  echo ""
+  exit
+fi
+if [[ "${_WORKERS}" = "true" ]]; then
+  listNodes "worker" ${TMP_OUT_FILE}
+  echo -e "${_CLR_YELLOW}Nodes for worker:${_CLR_NC}"
+  cat ${TMP_OUT_FILE}
+  rebootNodes ${TMP_OUT_FILE}
+  rm ${TMP_OUT_FILE}
+fi
 
 if [[ "${_CTRL_PLANE}" = "true" ]]; then
   listNodes "master" ${TMP_OUT_FILE}
   echo ""
-  echo "Nodes for master:"
+  echo -e "${_CLR_YELLOW}Nodes for master:${_CLR_NC}"
 
   cat ${TMP_OUT_FILE}
   rebootNodes ${TMP_OUT_FILE}
